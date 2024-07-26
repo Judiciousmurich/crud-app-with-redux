a3de1b408a10539968e0a69f5abaa0db8d7f9e87
judiciousmurich <murichjudicious45@gmail.com>
2024-07-26 15:31:30 +0300

    feat:add users

--- jsonplaceholder/src/features/users/UserList.jsx
+++ jsonplaceholder/src/features/users/UserList.jsx
@@ -1,9 +1,14 @@
  import React from 'react'
  
  const UserList = () => {
-  const users = [];
+  const users = [
+    { id: 1, name: 'Judy', email:'judy@gmail.com'},
+    {id:2, name:'Amos',email:'amos@gmail.com'}
+  ];
+  const renderCard = () => <div>Users here</div>
    return (
-     <div className="grid gap 5 md:grid-cols-2">UserList</div>
+     <div className="grid gap 5 md:grid-cols-2">  {users.length ? renderCard() : <p className='text-center col-span-2 text-gray-700 font-semibold'> No User</p>  }</div>
+    
    )
  }
  
