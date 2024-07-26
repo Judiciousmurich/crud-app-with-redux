8638eb8c8cdfc4bff173fb59542d0edc78a346dd
judiciousmurich <murichjudicious45@gmail.com>
2024-07-26 14:54:51 +0300

    feat:add styles

---
+++ jsonplaceholder/src/features/users/UserList.jsx
@@ -0,0 +1,10 @@
+ import React from 'react'
+ 
+ const UserList = () => {
+  const users = [];
+   return (
+     <div className="grid gap 5 md:grid-cols-2">UserList</div>
+   )
+ }
+ 
+ export default UserList
--- jsonplaceholder/tailwind.config.js
+++ jsonplaceholder/tailwind.config.js
@@ -1,8 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+export default {
+  content: [
+    "./src/**/*.html",
+    "./src/**/*.js",
+    "./src/**/*.jsx",
+    "./src/**/*.ts",
+    "./src/**/*.tsx",
+    "./src/**/*.",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
