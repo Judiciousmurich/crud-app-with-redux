bc55263a6918eb0ffc6f166954e0ca88cebde3ba
judiciousmurich <murichjudicious45@gmail.com>
2024-07-26 16:59:29 +0300

    feat:add users

--- jsonplaceholder/src/App.jsx
+++ jsonplaceholder/src/App.jsx
@@ -4,7 +4,8 @@ function App() {
 
   return (
 
-    <div>
+    <div className="container mx-auto px-2 max-w-5xl pt-10 md:pt-32">
+      <h1 className="text-center font-bold text-2xl text-gray-700" >CRUD App With Redux</h1>
       <UserList/>
     </div>
 
