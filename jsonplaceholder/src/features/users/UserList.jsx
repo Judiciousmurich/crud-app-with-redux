116d3c133d1754633bf73498a62d3035fb69f054
judiciousmurich <murichjudicious45@gmail.com>
2024-07-26 15:34:03 +0300

    feat:add users

--- jsonplaceholder/src/features/users/UserList.jsx
+++ jsonplaceholder/src/features/users/UserList.jsx
@@ -5,7 +5,9 @@
     { id: 1, name: 'Judy', email:'judy@gmail.com'},
     {id:2, name:'Amos',email:'amos@gmail.com'}
   ];
-  const renderCard = () => 
+  const renderCard = () => users.map(user =>(
+
+  ))
    return (
      <div className="grid gap 5 md:grid-cols-2">  {users.length ? renderCard() : <p className='text-center col-span-2 text-gray-700 font-semibold'> No User</p>  }</div>
     
