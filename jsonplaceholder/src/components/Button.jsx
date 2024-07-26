c9da326b8730ea1d1fbbf0131626fb99baffb714
judiciousmurich <murichjudicious45@gmail.com>
2024-07-26 17:08:58 +0300

    feat:add users

--- jsonplaceholder/src/components/Button.jsx
+++ jsonplaceholder/src/components/Button.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Button = ({onClick, children}) => {
   return (
-<button>
-    
+<button className='bg-indigo-600 text-white py-2 px-6 my-10 rounded hover:bg-indigo-700' onClick={onClick}
+>
+    {children}
+
 </button>
   )
 }
