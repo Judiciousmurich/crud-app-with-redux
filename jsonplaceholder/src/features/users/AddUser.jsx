af73413e33f074518343198418f0f7132b61ffee
judiciousmurich <murichjudicious45@gmail.com>
2024-08-04 17:06:46 +0300

    feat:add input

--- jsonplaceholder/src/features/users/AddUser.jsx
+++ jsonplaceholder/src/features/users/AddUser.jsx
@@ -1,22 +1,39 @@
 import React, { useState } from 'react'
 import TextField from '../../components/TextField'
+import Button from '../../components/Button';
+
 
 const AddUser = () => {
   const[values, setValues] = useState({name:'', email:''});
+  const handleAddUser = () => {
+   setValues({name:'', email:''});
+   dispatchEvent(addUser({
+    id:uuidv4(),
+    name: values,name,
+    email:values.email
+   }));
+   NavigationPreloadManager('/')
+
+  }
   return (
-    <>
-    <TextField
+<div className='mt-10 max-w-xl mx-auto'>
+<TextField
         label="Name"
         value={values.name}
         onChange={(e) => setValues({ ...values, name: e.target.value })}
         inputProps={{ type: 'text', placeholder: 'Jhon Doe' }}/>
+         <br />
         <TextField 
         label= "Email"
         value={values.email}
-        onChange={(e) => setValues({...values, name:e.target.value})}
-        inputProps={{ type:'text', placeholder: 'judy@gmail.com'}}
+        onChange={(e) => setValues({...values, email:e.target.value})}
+        inputProps={{ type:'email', placeholder: 'judy@gmail.com'}}
         />
-    </>
+        <Button onClick={handleAddUser}>Submit</Button>
+
+</div>
+   
+    
 
   )
 }
