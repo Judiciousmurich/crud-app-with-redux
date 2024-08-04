6d1e575aef8eeeb4928c5e22328da6ddd5f4cfe2
judiciousmurich <murichjudicious45@gmail.com>
2024-08-05 00:19:33 +0300

    feat:add input

--- jsonplaceholder/src/features/users/AddUser.jsx
+++ jsonplaceholder/src/features/users/AddUser.jsx
@@ -1,41 +1,38 @@
-import React, { useState } from 'react'
-import TextField from '../../components/TextField'
-import Button from '../../components/Button';
-
+import React, { useState } from "react";
+import TextField from "../../components/TextField";
+import Button from "../../components/Button";
 
 const AddUser = () => {
-  const[values, setValues] = useState({name:'', email:''});
+  const [values, setValues] = useState({ name: "", email: "" });
   const handleAddUser = () => {
-   setValues({name:'', email:''});
-   dispatchEvent(addUser({
-    id:uuidv4(),
-    name: values,name,
-    email:values.email
-   }));
-   NavigationPreloadManager('/')
-
-  }
+    setValues({ name: "", email: "" })
+    dispatchEvent(
+      addUser({
+        id: uuidv4(),
+        name: values,
+        email: values.email,
+      })
+    );
+    NavigationPreloadManager("/");
+  };
   return (
-<div className='mt-10 max-w-xl mx-auto'>
-<TextField
+    <div className="mt-10 max-w-xl mx-auto">
+      <TextField
         label="Name"
         value={values.name}
         onChange={(e) => setValues({ ...values, name: e.target.value })}
-        inputProps={{ type: 'text', placeholder: 'Jhon Doe' }}/>
-         <br />
-        <TextField 
-        label= "Email"
+        inputProps={{ type: "text", placeholder: "judy" }}
+      />
+      <br />
+      <TextField
+        label="Email"
         value={values.email}
-        onChange={(e) => setValues({...values, email:e.target.value})}
-        inputProps={{ type:'email', placeholder: 'judy@gmail.com'}}
-        />
-        <Button onClick={handleAddUser}>Submit</Button>
+        onChange={(e) => setValues({ ...values, email: e.target.value })}
+        inputProps={{ type: "email", placeholder: "judy@gmail.com" }}
+      />
+      <Button onClick={handleAddUser}>Submit</Button>
+    </div>
+  );
+};
 
-</div>
-   
-    
-
-  )
-}
-
-export default AddUser
+export default AddUser;
