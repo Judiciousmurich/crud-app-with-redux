e0fac27182290a0ca2248e580c4659259e84a51f
judiciousmurich <murichjudicious45@gmail.com>
2024-08-08 14:15:04 +0300

    feat:add edit user

--- jsonplaceholder/src/main.jsx
+++ jsonplaceholder/src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from 'react';
+import * as ReactDOMClient from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
+
+const container = document.getElementById('root');
+// Create a root.
+const root = ReactDOMClient.createRoot(container);
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
