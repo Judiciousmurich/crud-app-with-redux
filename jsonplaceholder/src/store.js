088206bab118dae8cc6957b7537fdc95ccd75e38
judiciousmurich <murichjudicious45@gmail.com>
2024-08-08 14:55:19 +0300

    feat:add edit user

--- jsonplaceholder/src/store.js
+++ jsonplaceholder/src/store.js
@@ -1,6 +1,6 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
-// import usersReducer from './features/users/userSlice';
+import usersReducer from './features/users/userSlice';
 
 const store = configureStore({
   reducer: {
