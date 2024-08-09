cd72b9c3018f56cbcfcbbfbc48633c209bb7e5eb
judiciousmurich <murichjudicious45@gmail.com>
2024-08-09 14:54:46 +0300

    feat:add tables values

--- jsonplaceholder/src/main.jsx
+++ jsonplaceholder/src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Update the import statement
+import ReactDOM from 'react-dom/client'; 
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
