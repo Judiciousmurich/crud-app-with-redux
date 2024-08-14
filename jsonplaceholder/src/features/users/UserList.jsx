1d998432178cbc0024ee1ccce77f4d70ec179e5a
judiciousmurich <murichjudicious45@gmail.com>
2024-08-14 17:42:27 +0300

    feat: add styles

--- jsonplaceholder/src/features/users/UserList.jsx
+++ jsonplaceholder/src/features/users/UserList.jsx
@@ -12,7 +12,7 @@ const UserList = () => {
   }
 
   const renderCard = () => users.map(user => (
-    <div className="bg-gray-300 p-5 flex items-center justify-between" key={user.id}>
+    <div className="bg-gray-300 p-5 flex items-center justify-between flex " key={user.id}>
       <div>
         <h3 className="font-bold text-lg text-gray-700">{user.name}</h3>
         <span className="font-normal text-gray-600">{user.email}</span>
